bfd014f941df0435530ba617b9ed2ccc22683a30
David Durieux <d.durieux@siprossii.com>
2018-07-24 08:41:46 +0200

    Enhance menu

--- src/pages/glpi/menu/menu.ts
+++ src/pages/glpi/menu/menu.ts
@@ -1,7 +1,11 @@
-import { Component } from "@angular/core";
+import {Component} from "@angular/core";
+
 import { BackendGlpiService } from "../../../backends/backend.glpi.service";
 
 import { TranslateService } from "@ngx-translate/core";
+import { Nav } from "ionic-angular";
+import {TicketPage} from "../ticket/ticket";
+
 
 @Component({
   providers: [ BackendGlpiService ],
@@ -12,29 +16,64 @@ import { TranslateService } from "@ngx-translate/core";
 export class GlpiMenu {
 
   public pages = [];
-  public itemtypes = {};
+  public itemtypes = [];
+  public submenu = [];
+  public currentmenu = "";
+  public onlyWithData = true;
 
   constructor(private httpGlpiService: BackendGlpiService, translate: TranslateService) {
-    this.itemtypes = {
-      Computer: "Computers",
-      Monitor: "Monitors",
-      Software: "Softwares",
-      NetworkEquipment: "Network devices",
-      Peripheral: "Devices",
-      Printer: "Printers",
-      Phone: "Phones",
-    };
-    this.loadMenu();
+    this.onlyWithData = true;
+    this.currentmenu = "";
+    this.submenu = [
+      {type: "asset", name: "Assets"},
+      {type: "assistance", name: "Assistance"},
+      {type: "management", name: "Management"},
+      {type: "tool", name: "Tools"},
+    ];
+    this.itemtypes = [
+      {order: 0, submenu: "asset", type: "Computer", name: "Computers"},
+      {order: 1, submenu: "asset", type: "Monitor", name: "Monitors"},
+      {order: 2, submenu: "asset", type: "Software", name: "Softwares"},
+      {order: 3, submenu: "asset", type: "NetworkEquipment", name: "Network devices"},
+      {order: 4, submenu: "asset", type: "Peripheral", name: "Devices"},
+      {order: 5, submenu: "asset", type: "Printer", name: "Printers"},
+      {order: 6, submenu: "asset", type: "CartridgeItem", name: "Cartridges"},
+      {order: 7, submenu: "asset", type: "ConsumableItem", name: "Consumables"},
+      {order: 8, submenu: "asset", type: "Phone", name: "Phones"},
+      {order: 9, submenu: "asset", type: "Rack", name: "Racks"},
+      {order: 10, submenu: "asset", type: "Enclosure", name: "Enclosures"},
+      {order: 11, submenu: "asset", type: "PDU", name: "PDUs"},
+
+      {order: 0, submenu: "assistance", type: "Ticket", name: "Tickets", component: TicketPage},
+      {order: 1, submenu: "assistance", type: "Problem", name: "Problems"},
+      {order: 2, submenu: "assistance", type: "Change", name: "Changes"},
+      {order: 3, submenu: "assistance", type: "Planning", name: "Planning"},
+      {order: 4, submenu: "assistance", type: "Stat", name: "Statistics"},
+      {order: 5, submenu: "assistance", type: "TicketRecurrent", name: "Recurrent tickets"},
+    ];
   }
 
-  private loadMenu() {
-    for (let itemtype in this.itemtypes) {
-      this.httpGlpiService.search(itemtype, [1, 2, 80], [], "0-1")
-        .subscribe(function(data) {
-          if (data.totalcount > 0) {
-            this.pages.push({name: this.itemtypes[itemtype], number: data.totalcount});
-          }
-        }.bind(this));
+  public loadMenu(submenu) {
+    this.currentmenu = submenu;
+    this.pages = [];
+    for (const datatype of this.itemtypes) {
+      if (datatype.submenu === submenu) {
+        this.pages.push({name: datatype.name, number: 0, order: datatype.order});
+        this.httpGlpiService.search(datatype.type, [1, 2, 80], [], "0-1")
+          .subscribe(function(data) {
+            this.pages[(datatype.order)].number = data.totalcount;
+          }.bind(this));
+      }
     }
   }
+
+  public return() {
+    this.pages = [];
+    this.currentmenu = "";
+  }
+
+  public goToPage(page) {
+//    MyApp.nav.setRoot(page.component);
+  }
+
 }
